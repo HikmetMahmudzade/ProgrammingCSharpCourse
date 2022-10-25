c5a3c1115cda9ef878f960ef3307700e3fe5fe86
HikmetMahmudzade <mahmudzadehikmet0@gmail.com>
2022-10-25 17:07:41 +0400

    New git addid

---
+++ FirstLessonProgramming/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FirstLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded1 daxil edin : ");
+
+            int eded1;
+
+            while (true)
+            {
+                try
+                {
+                    eded1 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ancaq int tipli simvol daxil edin!");
+                }
+            }
+
+            Console.WriteLine("Eded2 daxil edin : ");
+            int eded2 = int.Parse(Console.ReadLine());
+
+            //int sum = Sum(eded1, eded2);
+
+            Console.WriteLine("Cavab : " + Sum(eded1, eded2));
+        }
+
+        public static int Sum(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
